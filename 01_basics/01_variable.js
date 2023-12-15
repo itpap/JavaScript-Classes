8e9079eefd19fb5f832413bff073a5cbb838846f
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-15 06:01:05 +0000

    code for basics

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "prashantpawar141220@gmail.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+let accountstate
+
+/* 
+
+by defult variables is undefined 
+//accountId = 2 //not allowed
+
+*/
+accountEmail = "hc@hc.com"
+accountPassword = "1513513"
+accountcity = -"jodhpur"
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountcity, accountstate])
+
+/*
+do not use var
+because  of issues in block scope and functional scope
+*/
