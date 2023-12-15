6f41247d46f9611f572f4dc60507ab630946da53
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-15 06:01:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict ";  //treat all js code as newer version
+
+
+console.log(6+6)
+
+let name = "prashnat"
+let age =23
+let isloggedIn = false
+let state = null
+
+// number => 2to power53
+// bigint
+// string =>""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqe
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
