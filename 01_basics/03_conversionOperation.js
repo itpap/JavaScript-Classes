e57be7e2ae859eccb60ca433c52ea39838d5fe0a
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-15 06:34:57 +0000

    h

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+/*let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+ 
+"33" =>33
+"33abs" => NaN
+true => 1; false =>0
+null => 0
+undefined => */
+/*
+let isloggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+1 => true
+0 => false
+"" =>false
+"hitesh" =>true
+*/
+
+let someNumber = ss
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
