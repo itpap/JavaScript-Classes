6847c8d87df8e3f5b069f0db531802625e36e4b7
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-15 10:33:33 +0000

    t

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,63 @@
+const score = 400
+// console.log(score);
+
+const balence = new Number(100)
+// console.log(balence);
+
+// console.log(balence.toString().length);
+// console.log(balence.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreeds = 100000
+// console.log(hundreeds.toLocaleString('en-In'))
+
+//+++++++++++++++++++ Mtahs +++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.7));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4.2,3,6,8,1));
+// console.log(Math.max(4.2,3,6,8,1));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1 ))  + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
