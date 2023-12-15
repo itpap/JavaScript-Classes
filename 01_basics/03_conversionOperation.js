9e9262d2cc963bbbd934ad0fe4f98576a33af697
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-15 08:30:40 +0000

    m

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,10 +25,69 @@ console.log(booleanIsLoggedIn);
 0 => false
 "" =>false
 "hitesh" =>true
-*/
 
-let someNumber = ss
+
+let someNumber = 55
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+
+// ********** operations********************************
+
+/*
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+
+
+let str1 = "hello"
+let str2 = " Prashnat"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+1)
+console.log(1+"2")
+console.log("1"+ 1 + 2)
+
+*/
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 =num2 =num3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
