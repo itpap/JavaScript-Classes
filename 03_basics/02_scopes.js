4bdbc7da2f1ffcf8ceed6d457fb29615458aed7d
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 09:36:38 +0000

    k

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+let a = 102 
+// const b = 20
+// var c = 30
+
+
+
+if(true){
+    let a = 10 
+    const b = 20
+    console.log("innner: ",a);
+}
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
