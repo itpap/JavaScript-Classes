e3dfe7de3e5bff49ef6df162a05878003837e06f
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 15:04:04 +0000

    y

---
+++ 05_iteration/one.js
@@ -0,0 +1,62 @@
+// //for 
+
+// for (let index = 0; index < 10; index++) {
+//     const element =  index;
+//     if(element ==5){
+//         console.log("this best num");
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(`tableof ${i}`);
+//     for (let j = 1; j < 10; j++) {
+//         console.log(i*j);
+//     }
+// }
+
+// let myarray = ["flash","ironman","batman"]
+// console.log(myarray.length);
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+    
+// }
+
+
+//brak and continue
+
+// for (let index = 0; index <=20; index++) {
+//     if (index==5) {
+//         console.log(`detected ${index}`);
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+
+for (let index = 0; index <=20; index++) {
+    if (index==5) {
+        console.log(`detected ${index}`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+// //while loop
+// let i =0
+// while(i<=10){
+//     console.log(`value of index ${i}`);
+//     i=i+2
+// }
+
+// let myarr = ["flas","ironman","arjjt sing"]
+// let arr=0
+// while (arr<myarr.length){
+//     console.log(`value is ${myarr[arr]}`);
+//     arr++
+// }
+
+let score = 9
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10);
