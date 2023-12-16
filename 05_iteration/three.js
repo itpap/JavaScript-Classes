b709716b826e99855202e63617f7f8417d0ee062
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 15:15:52 +0000

    fdf

---
+++ 05_iteration/three.js
@@ -0,0 +1,40 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr =[1,2,3,4,5,6]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "hello i am here"
+
+// for (const gr of greetings) {
+//     console.log(gr);
+// }
+
+
+//Map
+
+// const map =new Map()
+// map.set('IN', "India")
+// map.set('oN', "Inladia")
+// map.set('UN', "United State Of America")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ":=", value);
+// }
+
+
+
+const myObj = {
+    
+}
+
+
+
+
