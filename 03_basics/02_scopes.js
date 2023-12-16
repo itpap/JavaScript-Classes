d2ad83c9eb9faec74cc9f6feeb54eb5b1f13e690
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 09:55:05 +0000

    jj

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,11 +7,60 @@ let a = 102
 if(true){
     let a = 10 
     const b = 20
-    console.log("innner: ",a);
+    // console.log("innner: ",a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one( ) {
+    const username = "prahsnat"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// *********************************intresting ********************************
+console.log(addone(5));
+function addone(num) {
+    return num+1
+}
+
+
+
+console.log(addtwo(5));
+const addtwo = function(num) { //
+    return num+2
+}
+     //
+
+
+
+
+
+
+
+
