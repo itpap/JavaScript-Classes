a53576b3a408f41ac8530e296053d4094ce38089
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 06:50:20 +0000

    r

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -45,28 +45,14 @@ const users = [
     }
 ]
 
-console.log(users[1].email);
-console.log(tinderUser);
+// console.log(users[1].email);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-
-
-console.log(tinderUser.hasOwnProperty("isloggedin"));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 
-
-
-
-
+// console.log(tinderUser.hasOwnProperty("isloggedin"));
  
-
-
-
-
-
-
-
-
 
