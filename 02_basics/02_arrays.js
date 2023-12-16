b63400a7b4e27ea15fa473bb02a9cc2184320faf
Prashant Atmaram Pawar <104372381+itpap@users.noreply.github.com>
2023-12-16 05:00:17 +0000

    f

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,1 +1,40 @@
-const marvel_heros =
+const marvel_heros = ["thor", "iron-man", "spiderman","jjlojas","ajsh"]
+const dc_heros = ["superman", "ice-man", "superman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allheros = marvel_heros.concat(dc_heros)
+// console.log(allheros);
+
+const all_new_heros = [...marvel_heros,...dc_heros]
+
+// console.log(all_new_heros)
+
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Prashnat"));
+console.log(Array.from("Prashnat"));
+console.log(Array.from({name:"Prashnat"}));//intresting
+
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
+
+
+
